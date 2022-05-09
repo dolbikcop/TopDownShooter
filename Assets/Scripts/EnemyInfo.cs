d024aa6335aaf36660187f4a45d94104b001fca8
dolbikcop <polyabikka@gmail.com>
2022-05-09 17:38:15 +0500

    Сделала скрипт для обучения при старте игры и при перовй встрече с врагом.

--- Assets/Scripts/EnemyInfo.cs
+++ Assets/Scripts/EnemyInfo.cs
@@ -2,18 +2,50 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class EnemyInfo : MonoBehaviour
 {
+    public GameObject startScreen;
+    public GameObject wormScreen;
+    public GameObject trojanScreen;
+
+    private GameObject screen;
+
+    private void Start()
+    {
+        screen = Instantiate(startScreen, transform.position, Quaternion.identity);
+        Time.timeScale = 0f;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && Time.timeScale == 0f)
+        {
             Time.timeScale = 1f;
+            Destroy(screen);            
+        }
     }
+
+    private bool isWorm, isTrojan;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.GetComponent<Enemy>()){}
-            //Time.timeScale = 0f;
+        Enemy e = col.gameObject.GetComponent<Enemy>();
+        if (e)
+        {
+            if (e.gameObject.tag == "Worm" && !isWorm)
+            {
+                Time.timeScale = 0f;
+                screen = Instantiate(wormScreen, transform.position, Quaternion.identity);
+                isWorm = true;
+            } else if (e.gameObject.tag == "Trojan" && !isTrojan)
+            {
+                Time.timeScale = 0f;
+                screen = Instantiate(trojanScreen, transform.position, Quaternion.identity);
+                isTrojan = true;
+            }
+            
+        }
     }
 }
